b0c776e3610f0937a6b4433de2e770dcaa90c79f
Paul Ashour <paul.ashour@gmail.com>
2018-10-10 22:19:42 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,9 @@ def load_library(path)
   library = YAML.load_file(path)
   em_lib = { 'get_meaning' => {}, 'get_emoticon' => {} }
   library.each do |meaning, em_arr|
-    # em_arr e.g. [':']
+    # em_arr e.g. [">:-(", "ヽ(ｏ`皿′ｏ)ﾉ"]
+    em_lib['get_meaning'][em_arr[1]] = meaning
+    em_lib['get_emoticon'][em_arr[0]] = [em_arr[1]]
 end
 
 def get_japanese_emoticon
