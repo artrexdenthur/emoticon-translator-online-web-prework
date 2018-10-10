be11abd233354420a5fb3fba66f9a77936b9ce40
Paul Ashour <paul.ashour@gmail.com>
2018-10-10 22:22:53 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(path)
   library.each do |meaning, em_arr|
     # em_arr e.g. [">:-(", "ヽ(ｏ`皿′ｏ)ﾉ"]
     em_lib['get_meaning'][em_arr[1]] = meaning
-    em_lib['get_emoticon'][em_arr[0]] = [em_arr[1]]
+    em_lib['get_emoticon'][em_arr[0]] = em_arr[1]
   end
   em_lib
 end
